f3e51f238097eb81da9f3038c3340c74bb69e387
test <le.sch@live.de>
2023-11-16 21:13:26 +0100

    simple strategy

--- logic/strategy.cs
+++ logic/strategy.cs
@@ -2,18 +2,55 @@ using client_dotnet.models;
 
 public class Strategy
 {
-    public static Bet Decide(Table? table) {
+    public static Bet Decide(Table? table)
+    {
         // fold on null data
         if (table is null) return new Bet(0);
-        
-        var ourPlayer = table.players[table.activePlayer];
-        
-        
-        return new Bet(table.minimumBet);
-    }
-    
-    
-    
-    
-}
 
+        var ourPlayer = table.players[table.activePlayer];
+
+        foreach (var ourPlayerCard in ourPlayer.cards)
+        {
+            ourPlayerCard?.rateCard();
+        }
+
+        var higherCard = ourPlayer.cards[0];
+        var lowerCard = ourPlayer.cards[1];
+        if (ourPlayer.cards[0].points < ourPlayer.cards[1].points)
+        {
+            higherCard = ourPlayer.cards[1];
+            lowerCard = ourPlayer.cards[0];
+        }
+
+        float score = higherCard.points;
+
+        // Cards suited?
+        if (higherCard.rank == lowerCard.rank) score *= 2;
+
+        var gap = higherCard.getGap(lowerCard);
+
+        // Card gap
+        if (gap >= 4) score -= 5;
+        else if (gap == 3) score -= 4;
+        else if (gap == 2) score -= 2;
+        else if (gap == 1) score -= 1;
+        else if (gap == 0) score -= 0;
+
+        if (gap <= 1 && higherCard.rank != Rank.Q && higherCard.rank != Rank.K && higherCard.rank != Rank.A)
+            score = score + 1;
+        
+        if (score >= 10) return new Bet(table.minimumRaise);
+        
+        if (score >= 9 && table.activePlayer <= 1) return new Bet(table.minimumRaise);
+        if (score >= 8 && table.activePlayer <= 1) return new Bet(0);
+        
+        if (score >= 8 && table.activePlayer <= 3) return new Bet(table.minimumRaise);
+        if (score >= 7 && table.activePlayer <= 3) return new Bet(0);
+        
+        if (score >= 7 && table.activePlayer <= 5) return new Bet(table.minimumRaise);
+        if (score >= 6 && table.activePlayer <= 5) return new Bet(0);
+
+        return new Bet(0);
+
+    }
+}
--- models/card.cs
+++ models/card.cs
@@ -2,5 +2,50 @@ public class Card
 {
     public Rank rank;
     public Suit suit;
+    public float points = 0;
+    
+
+    public void rateCard()
+    {
+        if (Rank.A == rank) points = 10;
+        if (Rank.K == rank) points = 8;
+        if (Rank.Q == rank) points = 7;
+        if (Rank.J == rank) points = 6;
+        if (Rank._10 == rank) points = 5;
+        if (Rank._9 == rank) points = 9/2;
+        if (Rank._8 == rank) points = 4;
+        if (Rank._7 == rank) points = 7/2;
+        if (Rank._6 == rank) points = 3;
+        if (Rank._5 == rank) points = 5/2;
+        if (Rank._4 == rank) points = 2;
+        if (Rank._3 == rank) points = 3/2;
+        if (Rank._2 == rank) points = 1;
+    }
+    
+    public int getGap(Card card)
+    {
+        int gapNumber = card.getGapNumber();
+        int gapNumber2 = this.getGapNumber();
+        if (gapNumber > gapNumber2) return gapNumber - gapNumber2;
+        return gapNumber2 - gapNumber;
+    }
+
+    public int getGapNumber()
+    {
+        if (Rank.A == rank) return 13;
+        if (Rank.K == rank) return 12;
+        if (Rank.Q == rank) return 11;
+        if (Rank.J == rank) return 10;
+        if (Rank._10 == rank) return 9;
+        if (Rank._9 == rank) return 8;
+        if (Rank._8 == rank) return 7;
+        if (Rank._7 == rank) return 6;
+        if (Rank._6 == rank) return 5;
+        if (Rank._5 == rank) return 4;
+        if (Rank._4 == rank) return 3;
+        if (Rank._3 == rank) return 2;
+        if (Rank._2 == rank) return 1;
+        return 0;
+    }
 }
 
